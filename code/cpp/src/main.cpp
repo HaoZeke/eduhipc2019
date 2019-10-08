395075d60f56d7db76466930dff5ddb9deaa3468
Amrita Goswami <amrita16thaug646@gmail.com>
2019-10-09 04:34:24 +0530

    main: Now initializes the rdf array

--- code/cpp/src/main.cpp
+++ code/cpp/src/main.cpp
@@ -6,6 +6,7 @@
 // Internal Libraries
 #include <generic.hpp>
 #include <inputOutput.hpp>
+#include <rdf.hpp>
 
 
 int main() {
@@ -17,7 +18,9 @@ int main() {
   int stepGap = 1;                      // Gap between steps
   // Make equiliSteps one if you don't want to skip anything
   int equiliSteps = 50;                 // Number of steps to skip (last frame exclusive)
-
+  // RDF
+  double binsize = 0.01;                // In Angstroms
+  double cutoff = 12;                   // Cutoff for the RDF (should be less than half the box)
   // -------------------------------------------- // Variables
   int totalSteps = 0;                   // Starts from 1
   std::vector<int> box;                 // Box lengths
@@ -36,11 +39,24 @@ int main() {
   bool fillCoord;                       // true if the frame is to be processed
   std::vector<std::string> tokens;      // Vector containing word tokens
   std::vector<double> numbers;          // Vector containing type double numbers
+  // -------------------------------------------- // RDF Specific Variables
+  int nbin;                             // Number of bins
+  int switchVar;                        // equal to 0 for init, 1 for adding and 2 for final normalization
+  int nframes;                          // Number of frames; used for normalizing g(r)
   // -------------------------------------------- // Main logic
 
   // Read in the total number of steps.
   // In this case, nop does not change throughout
   io::getTotalTimesteps(lammpsInputTraj, &totalSteps);
+
+  // ----
+  // Allocate the RDF array
+  nbin = (int)(cutoff/binsize) + 1;
+  double rdf[nbin]; // init
+  // Initialize the RDF
+  switchVar = 0;
+  rdf::gr(rdf, &nframes, binsize, nbin, coord, nop, switchVar);
+  // ----
 
   // Check to make sure that the user has entered valid  steps
   if (equiliSteps+numCalcSteps*equiliSteps > totalSteps){
